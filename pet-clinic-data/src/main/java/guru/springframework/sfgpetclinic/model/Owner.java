0bfef2599f5e00b8a88199e4764177f7d680ae70
Diego <diegorl84@gmail.com>
2019-12-28 22:33:45 -0500

    Adding entity Visit and Owner set pets. Closes #26

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,8 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author diego
  * @since 23/11/2019
  */
 public class Owner extends Person {
+    private Set<Pet> pet;
+
+    public Set<Pet> getPet() {
+        return pet;
+    }
+
+    public void setPet(Set<Pet> pet) {
+        this.pet = pet;
+    }
 }
