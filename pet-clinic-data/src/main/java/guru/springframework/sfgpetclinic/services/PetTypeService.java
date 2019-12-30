8e1876c8297fa8436d7faedc9bcb1a3ca79227b1
Diego <diegorl84@gmail.com>
2019-12-29 22:36:58 -0500

    Adding PetTypeMap. Closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * @author diego
+ * @since 29/12/2019
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
