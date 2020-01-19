f39f092026b8353e437ca010fa371af9289399bb
Diego <diegorl84@gmail.com>
2020-01-18 19:15:58 -0500

    Map impl for visit service. Closes #46

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.services.map.AbstractMapService;
+
+/**
+ * @author diego
+ * @since 18/01/2020
+ */
+public interface VisitService extends CrudService<Visit,Long> {
+}
